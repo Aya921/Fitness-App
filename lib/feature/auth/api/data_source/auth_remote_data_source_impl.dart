d3ea7f517c5e0eb81c1044edbc79f64d3d6aa3aa
mariam2MOHMED <mariammohmed.25720@gmail.com>
2025-10-19 12:06:50 +0300

    solve an issue

--- lib/feature/auth/api/data_source/auth_remote_data_source_impl.dart
+++
@@ -1,5 +0,0 @@
-import 'package:injectable/injectable.dart';
-
-import '../../data/data_source/auth_remote_data_source.dart';
-@Injectable(as: AuthRemoteDataSource)
-class AuthRemoteDataSourceImpl implements AuthRemoteDataSource{}
