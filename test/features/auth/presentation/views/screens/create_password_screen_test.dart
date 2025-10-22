30086008d4742e41681190eb3f254571cf4e887d
Aya921 <aya44saber@gmail.com>
2025-10-22 15:30:20 +0300

    add wiget test of forgetPass screens

---
+++ test/features/auth/presentation/views/screens/create_password_screen_test.dart
@@ -0,0 +1,84 @@
+import 'package:fitness/core/l10n/translations/app_localizations.dart';
+import 'package:fitness/core/responsive/size_provider.dart';
+import 'package:fitness/core/widget/app_background.dart';
+import 'package:fitness/core/widget/blur_container.dart';
+import 'package:fitness/core/widget/logo.dart';
+import 'package:fitness/features/auth/presentation/views/screens/create_password_screen.dart';
+import 'package:fitness/features/auth/presentation/views/widgets/create_new_pass_section.dart';
+import 'package:fitness/features/auth/presentation/views/widgets/text_section.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('test create password screen structure ...', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: SizeProvider(
+          baseSize: Size(375, 812),
+          height: 812,
+          width: 375,
+          child: CreatePasswordScreen(),
+        ),
+      ),
+    );
+
+    final l10n = await AppLocalizations.delegate.load(const Locale('en'));
+    expect(find.byType(AppBackground), findsOneWidget);
+    expect(find.byType(SafeArea), findsOneWidget);
+    expect(find.byType(Column), findsAtLeast(1));
+    expect(find.byType(Logo), findsOneWidget);
+    expect(find.byType(Spacer), findsNWidgets(3));
+    expect(find.byType(TextSection), findsOneWidget);
+    expect(find.byType(BlurContainer), findsOneWidget);
+    expect(find.byType(Logo), findsOneWidget);
+    expect(find.byType(CreateNewPassSection), findsOneWidget);
+    expect(find.text(l10n.makeSure8Char), findsOneWidget);
+    expect(find.text(l10n.createNewPass), findsOneWidget);
+
+    expect(
+      find.byWidgetPredicate(
+        (widget) =>
+            widget is Column &&
+            widget.crossAxisAlignment == CrossAxisAlignment.start &&
+            widget.mainAxisAlignment == MainAxisAlignment.center &&
+            widget.children.length == 6 &&
+            widget.children[0] is Logo &&
+            widget.children[1] is Spacer &&
+            widget.children[2] is TextSection &&
+            widget.children[3] is BlurContainer &&
+            widget.children[4] is Spacer&&
+            widget.children[5] is Spacer,
+      ),
+      findsOneWidget,
+    );
+    expect(
+      find.byWidgetPredicate(
+        (widget) =>
+            widget is SafeArea &&
+            widget.child is Column
+      ),
+      findsOneWidget,
+    );
+    expect(
+      find.byWidgetPredicate(
+        (widget) =>
+            widget is AppBackground && widget.child is SafeArea
+           
+      ),
+      findsOneWidget,
+    );
+    expect(
+      find.byWidgetPredicate(
+        (widget) =>
+            widget is BlurContainer && widget.blurChild is CreateNewPassSection
+           
+      ),
+      findsOneWidget,
+    );
+   
+
+   
+  });
+}
---
+++ test/features/auth/presentation/views/widgets/otp_section_test.dart
@@ -0,0 +1,65 @@
+import 'package:fitness/core/extension/app_localization_extension.dart';
+import 'package:fitness/core/l10n/translations/app_localizations.dart';
+import 'package:fitness/core/responsive/size_helper.dart';
+import 'package:fitness/core/responsive/size_provider.dart';
+import 'package:fitness/core/widget/custum_fields_button.dart';
+import 'package:fitness/features/auth/presentation/views/widgets/custum_otp.dart';
+import 'package:fitness/features/auth/presentation/views/widgets/otp_section.dart';
+import 'package:fitness/features/auth/presentation/views/widgets/verification_question_section.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+ 
+
+  testWidgets('otp section test  structure', (WidgetTester tester) async {
+    await tester.pumpWidget(
+       MaterialApp(
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: SizeProvider(
+          baseSize: const Size(375, 812),
+          height: 812,
+          width: 375,
+          child: Scaffold(body: OtpSection()),
+        ),
+      ),
+    );
+
+    expect(find.byType(Column), findsAtLeastNWidgets(1));
+    expect(find.byType(CustomOtpField), findsAtLeastNWidgets(1));
+    expect(find.byType(SizedBox), findsAtLeastNWidgets(1));
+    expect(find.byType(CustumFieldsButton), findsAtLeastNWidgets(1));
+    expect(find.byType(VerifcationQuestionSection), findsAtLeastNWidgets(1));
+
+    final context = tester.element(find.byType(CustomOtpField));
+    expect(
+      find.byWidgetPredicate(
+        (widget) => widget is CustomOtpField && widget.numberOfFields ==4,
+      ),
+      findsOneWidget,
+    );
+    expect(
+      find.byWidgetPredicate(
+        (widget) => widget is SizedBox &&widget.height==context.setHight(30),
+      ),
+      findsOneWidget,
+    );
+    expect(
+      find.byWidgetPredicate(
+        (widget) => widget is CustumFieldsButton &&widget.text==context.loc.confirm
+      ),
+      findsOneWidget,
+    );
+    expect(
+      find.byWidgetPredicate(
+        (widget) => widget is Column &&widget.children.length==4&&widget.children[0] is CustomOtpField&&
+        widget.children[1] is SizedBox&&
+        widget.children[2] is CustumFieldsButton&&
+        widget.children[3] is VerifcationQuestionSection
+      ),
+      findsOneWidget,
+    );
+   
+  });
+}
