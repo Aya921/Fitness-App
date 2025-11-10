009ab8582c3c713bb6c4fc6497237b7b8fc2c842
mariam2MOHMED <mariammohmed.25720@gmail.com>
2025-11-10 23:16:53 +0200

    add testing

--- lib/features/smart_coach/presentation/view/widgets/user_profile_section.dart
+++ lib/features/smart_coach/presentation/view/widgets/user_profile_section.dart
@@ -3,18 +3,16 @@ import 'package:fitness/core/extension/app_localization_extension.dart';
 import 'package:fitness/core/responsive/size_helper.dart';
 import 'package:fitness/core/theme/app_colors.dart';
 import 'package:fitness/core/theme/font_style.dart';
-import 'package:fitness/core/user/user_manager.dart';
 import 'package:fitness/core/widget/custom_pop_icon.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 
 class UserProfileSection extends StatelessWidget {
-  const UserProfileSection({super.key});
-
+  const UserProfileSection({super.key,required this.firstName});
+final String firstName;
   @override
   Widget build(BuildContext context) {
-    final user=UserManager().currentUser!.personalInfo!;
     return Padding(
       padding: EdgeInsetsDirectional.symmetric(
         horizontal: context.setWidth(20),
@@ -28,7 +26,7 @@ class UserProfileSection extends StatelessWidget {
             TextSpan(
               children: [
                 TextSpan(
-                  text: "${context.loc.hiHomeText} ${user.firstName}\n",
+                  text: "${context.loc.hiHomeText} $firstName\n",
 
                   style: getMediumStyle(
                     color: AppColors.white,
